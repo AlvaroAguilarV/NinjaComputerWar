a1068bc482d579951d8da17321cce04aa640f6ea
dam1b <dam1b@aula15-12.JUANXXIII.net>
2017-06-01 20:36:00 +0200

    Version 1.0.0.1

--- src/net/juanxxiii/j23gameengine/modelo/EnemigoA.java
+++ src/net/juanxxiii/j23gameengine/modelo/EnemigoA.java
@@ -5,10 +5,21 @@
  */
 package net.juanxxiii.j23gameengine.modelo;
 
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author dam1b
  */
 public class EnemigoA extends Enemigo{
+
+    public EnemigoA(float vida, float danyo, float velocidad, int coordX, int coordY, BufferedImage imagen) {
+        super(vida, danyo, velocidad, coordX, coordY, imagen);
+    }
+
+    @Override
+    public void recibirImpacto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
